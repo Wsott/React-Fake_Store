121e86118f988a4ca3336fb7aae23be8b15392b1
Emiliano Gomez Olivera <theguiltyspark343@outlook.com>
2023-07-15 20:42:17 -0300

    Correccion de error

--- src/context/UserProvider.tsx
+++ src/context/UserProvider.tsx
@@ -1,6 +1,16 @@
 import { createContext, useState } from "react";
 
-const UserContext = createContext("");
+// const UserContext = createContext<string>("");
+
+interface UserContextType {
+  user: string;
+  setUser: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const UserContext = createContext<UserContextType>({
+  user: "",
+  setUser: () => {return},
+});
 
 export const UserProvider = ({ children }: any) => {
     const [user, setUser] = useState("");
