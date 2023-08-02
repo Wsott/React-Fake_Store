cdaa336780cb69617b87bf34908918e614101a55
Emiliano Gomez Olivera <theguiltyspark343@outlook.com>
2023-08-02 17:20:52 -0300

    Correccion para el deploy

--- src/pages/CheckoutFinished.tsx
+++ src/pages/CheckoutFinished.tsx
@@ -1,7 +1,7 @@
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 import CartContext from "../context/CartProvider";
 import style from "../styles/pages.module.css";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function CheckoutFinished () {
     const { cleanTheCart } = useContext(CartContext);
