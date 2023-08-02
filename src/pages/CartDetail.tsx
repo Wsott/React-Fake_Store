6ee6c8ea18ffe8c35b4760fe018a9b487e59c0b4
Emiliano Gomez Olivera <theguiltyspark343@outlook.com>
2023-08-02 17:48:29 -0300

    Correccion para el deploy

--- src/pages/CartDetail.tsx
+++ src/pages/CartDetail.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 import CartContext from "../context/CartProvider";
 import CheckoutRow from "../components/unique/CheckoutRow";
 import style from "../styles/pages.module.css";
